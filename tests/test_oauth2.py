a6df26b5bdf1ee571b1b96abef8745db3a96e574
Matt Dodge <matt@neutral.io>
2014-10-20 12:14:04 -0700

    Starting off the OAuth2 Mixin - only has JWT support for now

---
+++ tests/test_oauth2.py
@@ -0,0 +1,36 @@
+from ..oauth2 import OAuth2, OAuth2Exception
+from nio.common.block.base import Block
+from nio.util.support.block_test_case import NIOBlockTestCase
+from nio.modules.threading import sleep
+
+
+class OAuthBlock(OAuth2, Block):
+    pass
+
+
+class TestOAuth2Mixin(NIOBlockTestCase):
+
+    def test_token(self):
+        block = OAuthBlock()
+        self.configure_block(block, {
+            "key_config_file": "tests/KEY_FILE_GOES_HERE.json"
+        })
+
+        token = block.get_access_token(
+            'https://www.googleapis.com/auth/analytics.readonly')
+
+        print(token.token)
+        sleep(300)
+        token.refresh()
+        print(token.token)
+
+        self.assertIsNotNone(token)
+
+    def test_bad_scope(self):
+        block = OAuthBlock()
+        self.configure_block(block, {
+            "key_config_file": "tests/GoBuffs-e254fba1cd4a.json"
+        })
+
+        with self.assertRaises(OAuth2Exception):
+            block.get_access_token('not-a-scope')
