7fd1cff4ffb68b893cdb318b96196043b4735b5d
Oren Leiman <oleiman@neutral.io>
2015-01-05 13:59:38 -0800

    BLK-238 Mock out the token request in the tests
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

--- tests/test_oauth2.py
+++ tests/test_oauth2.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, MagicMock
+from requests import Response
 from ..oauth2 import OAuth2, OAuth2Exception
 from nio.common.block.base import Block
 from nio.util.support.block_test_case import NIOBlockTestCase
@@ -9,7 +11,22 @@ class OAuthBlock(OAuth2, Block):
 
 class TestOAuth2Mixin(NIOBlockTestCase):
 
-    def test_token(self):
+    @patch.object(OAuth2, '_load_json_file',
+                  return_value={'client_email': 'foo@bar.gov',
+                                'private_key': 'WhatAKey'})
+    @patch('requests.post')
+    @patch('oauth2client.client.SignedJwtAssertionCredentials.'
+           '_generate_refresh_request_body')
+    @patch('oauth2client.client.SignedJwtAssertionCredentials.'
+           '_generate_refresh_request_headers')
+    def test_token(self, mock_head, mock_bod, mock_post, mock_load):
+        
+        # mock out the token request response
+        the_response = Response()
+        the_response.status_code = 200
+        the_response.json = MagicMock(return_value={'access_token': 'foobar'})
+        mock_post.return_value = the_response
+        
         block = OAuthBlock()
         self.configure_block(block, {
             "key_config_file": "tests/KEY_FILE_GOES_HERE.json"
@@ -18,7 +35,7 @@ class TestOAuth2Mixin(NIOBlockTestCase):
         token = block.get_access_token(
             'https://www.googleapis.com/auth/analytics.readonly')
 
-        self.assertIsNotNone(token)
+        self.assertEqual(token, 'foobar')
 
     def test_bad_scope(self):
         block = OAuthBlock()
